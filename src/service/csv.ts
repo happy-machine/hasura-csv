9948c775d562c19368fb64435b3bcb602988c39d
Dan Stein <dan.stein@generalbioinformatics.com>
2020-11-06 20:29:32 +0000

    ..

--- src/service/csv.ts
+++ src/service/csv.ts
@@ -9,20 +9,16 @@ export const csvWriter = async (filename, keys, timestamp) => {
   const path = `${OUTPUT_DIR}${filename}`;
   try {
     await fsPromises.writeFile(path, "", { flag: "wx" });
-    logger.info(`'${path}' written. with columns: ${keys}`);
   } catch (e) {
     if (!e.code.includes("EEXIST")) {
       logger.error(e);
-    } else {
-      logger.info(`'${path}' written. with columns: ${keys}`);
     }
   }
-  console.log({keys})
-  const out =  createObjectCsvWriter({
+  const out = createObjectCsvWriter({
     path,
     header: keys.map((key) => ({ id: key, title: key })),
-    append: currentTimeStamp == timestamp ? true : false
+    append: currentTimeStamp == timestamp ? true : false,
   });
   currentTimeStamp = timestamp;
-  return out
+  return out;
 };
