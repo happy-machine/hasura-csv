2014dc26583ace237bd92c7ce0d141629f30a591
Dan Stein <dan.stein@generalbioinformatics.com>
2020-11-06 20:28:25 +0000

    ..

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ dotenv.config({ path: "./src/etc/.env" });
 export const OUTPUT_DIR = process.env.OUTPUT_DIR;
 export const NODE_ENV = process.env.NODE_ENV;
 export const HOST = process.env.HOST;
+export const MODE = process.env.MODE;
 export const PORT = Number(process.env.PORT);
 export const DB = process.env.DB;
 export const FLUENTD_HOST = process.env.FLUENTD_HOST;
